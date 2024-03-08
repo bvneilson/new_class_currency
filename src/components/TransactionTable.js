ff48484f40b5caf32dc108c6d3dee19a965d53d1
Brennan Neilson <bvneilson@users.noreply.github.com>
2024-03-08 08:47:03 -0700

    removing transaction mini menu until functional

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -22,43 +22,6 @@ function TransactionTable({ transactions }) {
             <div className="text-sm font-medium leading-6 text-gray-900">
               {transaction.reason}
             </div>
-            <Menu as="div" className="relative ml-auto">
-              <Menu.Button className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500">
-                <span className="sr-only">Open options</span>
-                <EllipsisHorizontalIcon
-                  className="h-5 w-5"
-                  aria-hidden="true"
-                />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        to={`/transactions/${transaction.id}`}
-                        className={classNames(
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900"
-                        )}
-                      >
-                        View / Edit
-                        <span className="sr-only">
-                          , {transaction.transaction_name}
-                        </span>
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </Menu>
           </div>
           <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
